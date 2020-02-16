6a4e7847f13bfc50822661aabdd4007ac0df97ea
曾增 <32710562+whynotwilson@users.noreply.github.com>
2020-02-16 21:24:18 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const restaurants = require('./restaurant.json')
+const app = express()
+const port = 3000
+const exphbs= require('express-handlebars')
+
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public'))
+
+//routes setting
+app.get('/', (req, res) => {
+    const restaurantList = restaurants.results
+    res.render('index', {restaurantList: restaurantList })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+    const id = req.params.id
+    const restaurant = restaurants.results
+    console.log(restaurant[Number(id)-1])
+    res.render('show', {restaurant: restaurant[Number(id)-1]})
+})
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    const restaurantList = restaurants.results.filter(restaurant => 
+        {return restaurant.name.toLowerCase().includes(keyword.toLowerCase())}
+    )
+    res.render('index', {restaurantList: restaurantList, keyword: keyword})
+})
+
+app.listen(port, () => {
+    console.log(`My_restaurant server is running......`)
+})
