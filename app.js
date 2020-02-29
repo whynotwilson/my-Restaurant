455ab21fcfdb107318427b2ad884329c9642e2d9
Wilson <p780729@hotmail.com>
2020-03-01 01:11:22 +0800

    Update index
    add edit & detail & delete link

--- app.js
+++ app.js
@@ -3,15 +3,36 @@ const restaurants = require('./restaurant.json')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-// setting template engine
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting static files
 app.use(express.static('public'))
 
+// 資料庫連線
+mongoose.connect('mongodb://localhost/restaurant', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+// 資料庫連線後，透過 mongoose.connection 拿到 Connection 的物件
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongoDB errer')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
+
 // setting route
+// 首頁
 app.get('/', (req, res) => {
   const restaurantList = restaurants.results
   res.render('index', { restaurantList })
