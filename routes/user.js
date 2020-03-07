963a6d7eef72e4f7822242a028924485be16c18d
Wilson <p780729@hotmail.com>
2020-03-08 03:01:09 +0800

    Add user registration feature

--- routes/user.js
+++ routes/user.js
@@ -17,6 +17,60 @@ router.get('/register', (req, res) => {
 })
 
 // 註冊檢查
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body
+
+  // 錯誤訊息提示
+  const errors = []
+
+  if (password !== password2) {
+    errors.push({ message: '二次密碼不一致' })
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  } else {
+    // 尋找 email 是否已被註冊
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ message: 'Email 已被使用' })
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        })
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+            newUser
+              .save()
+              .then(user => {
+                res.redirect('/')
+              })
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
+  }
+})
 
 // 登出
 
